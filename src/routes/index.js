313cb5ee0b96f1ef312c832af28bc783e7844c8d
victornwaiwu <victor.nwaiwu@andela.com>
2019-02-26 16:00:04 +0100

    refactor: use lazy loading of components in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(Router);
+
+function loadView(view) {
+  return () => import(`../views/${view}.vue`);
+}
 
 export default new Router({
   mode: 'history',
@@ -19,7 +23,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: loadView('About'),
+    },
+    {
+      path: '/register',
+      name: 'signup',
+      component: loadView('SignUpView'),
     },
   ],
 });
