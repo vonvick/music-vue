87f311e037fab9e27829221e5651d3e7101bd965
victornwaiwu <victor.nwaiwu@andela.com>
2019-02-26 15:59:25 +0100

    feature: create instance properties to be used across app

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import router from './routes';
 import store from './store';
 import './registerServiceWorker';
 
+// Styles
+import './assets/scss/style.scss';
+
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+Vue.prototype.$appName = 'Vonvick Music';
+Vue.prototype.$api = process.env.VUE_APP_API_URL;
 
 new Vue({
   router,
