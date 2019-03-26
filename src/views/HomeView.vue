78d858650e1dd27b09f397867c65bd0d50823c09
victornwaiwu <victor.nwaiwu@andela.com>
2019-03-26 15:54:45 +0100

    feature: add test logout method to homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <button class="btn btn-default" @click="logout">LOG OUT</button>
   </div>
 </template>
 
 <script>
+// Mixins
+import requestMixin from '@/mixins/request-mixin';
 export default {
   name: 'home-page-view',
+  mixins: [requestMixin],
   components: {},
+  data() {
+    return {
+      logoutUrl: `${this.$api}/auth/sign_out`,
+    }
+  },
+  methods: {
+    async logout() {
+      await this.deleteRequest(this.logoutUrl);
+
+      await this.$store.dispatch('user/clearUserDetailsData',[
+        'email',
+        'firstName',
+        'lastName',
+        'username',
+        'description',
+        'avatar',
+        'roleId',
+        'headline',
+        'accessToken',
+        'clientToken',
+        'expiry'
+      ]);
+
+      this.$router.push('/login');
+    },
+  },
 };
 </script>
 <style lang="scss" scoped></style>
